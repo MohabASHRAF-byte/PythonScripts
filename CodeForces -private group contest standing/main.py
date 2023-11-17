2a2f58e430b2aaf200546a8947134ccfcedd529c
Mohab Ashraf <63811158+MohabASHRAF-byte@users.noreply.github.com>
2023-11-18 00:09:38 +0200

    Update main.py

--- CodeForces -private group contest standing/main.py
+++ CodeForces -private group contest standing/main.py
@@ -1,15 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from time import sleep, time
 from random import randint as rand
 import openpyxl
 
 # Replace these with your Codeforces credentials
-username = "Mohab"
-password = "mohabashraf123456789"
-groupId = "V2UavMqsbA"
-contestId = "484385"
+username = "Manger_Handle"
+password = "Manger_password"
+groupId = "group_id"
+contestId = "contest_id"
 #
+haveHeader = False
+start_time = time()
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
+
+def doHeader(tot: int):
+    header = ["Rank", "User"]
+    for iii in range(tot):
+        header.append(chr(iii + ord('A')))
+    return header
+
+
 driver = webdriver.Chrome()
 driver.get("https://codeforces.com/enter")
 username_input = driver.find_element(By.ID, "handleOrEmail")
@@ -21,38 +34,37 @@ password_input.send_keys(password)
 sleep(1)
 submit_button.click()
 sleep(3)
-driver.get(f"https://codeforces.com/group/{groupId}/contest/{contestId}/standings/groupmates/true")
-sleep(2)
-users = driver.find_element(By.CLASS_NAME, "standings").find_elements(By.TAG_NAME, "tr")
-contestants = []
-print(len(users), users)
-for i in range(1, len(users) - 1):
-    user = users[i].find_elements(By.TAG_NAME, "td")
-    user_rank = user[0].text
-    user_name = user[1].find_element(By.CLASS_NAME, "rated-user").text
-    problems = []
-    for ii in range(4, len(user)):
-        problem = user[ii]
-        try:
-            isRejected = problem.find_element(By.CLASS_NAME, "cell-rejected")
-            problems.append(isRejected.text if isRejected.text != ' ' else 0)
-        except:
-            problems.append(problem.find_element(By.CLASS_NAME, "cell-accepted").text)
-    user_rank = user_rank if user_rank != ' ' else "*"
-    contestants.append([user_rank, user_name, problems])
-numOfProblems = len(contestants[0][2])
-header = ["Rank", "User"]
-for i in range(numOfProblems):
-    header.append(chr(i + ord('A')))
-print(header)
-workbook = openpyxl.Workbook()
-sheet = workbook.active
-sheet.append(header)
 
-for contestant in contestants:
-    li = [contestant[0], contestant[1]]
-    for problem in contestant[2]:
-        li.append(problem)
-    sheet.append(li)
+limit = 1
+for idx in range(1, limit + 1):
+    driver.get(f"https://codeforces.com/group/{groupId}/contest/{contestId}/standings/groupmates/true/page/{idx}")
+    sleep(2)
+    try:
+        limit = len(driver.find_element(By.CLASS_NAME, "custom-links-pagination").find_elements(By.TAG_NAME, "nobr"))
+    except:
+        pass
+    users = driver.find_element(By.CLASS_NAME, "standings").find_elements(By.TAG_NAME, "tr")
+    for i in range(1, len(users) - 1):
+        user = users[i].find_elements(By.TAG_NAME, "td")
+        user_rank = user[0].text
+        user_name = user[1].find_element(By.CLASS_NAME, "rated-user").text
+        problems = []
+        if not haveHeader:
+            haveHeader = True
+            sheet.append(doHeader(len(user) - 4))
+        for ii in range(4, len(user)):
+            problem = user[ii]
+            try:
+                isRejected = problem.find_element(By.CLASS_NAME, "cell-rejected")
+                problems.append(isRejected.text if isRejected.text != ' ' else 0)
+            except:
+                problems.append(problem.find_element(By.CLASS_NAME, "cell-accepted").text)
+        user_rank = user_rank if user_rank != ' ' else "*"
+        sheet.append([user_rank, user_name] + problems)
+
 workbook.save(f"contest-{contestId}.xlsx")
+print("Done !")
+end_time = time()
+elapsed_time = end_time - start_time
+print(f"\033[92mTime taken: {elapsed_time:.2f} seconds\033[0m")
 driver.quit()
